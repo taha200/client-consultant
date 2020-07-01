af1467b6ee06655e7195a4fd7427abd8e1b92671
taha200 <41546947+taha200@users.noreply.github.com>
2020-07-02 02:48:31 +0500

    int

--- android/app/src/main/java/com/thinksolutionz/consultancyapp/MainActivity.java
+++ android/app/src/main/java/com/thinksolutionz/consultancyapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "phscwellness";
+    return "PSHCWellness";
   }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
